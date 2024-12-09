77e38729263481a67d7fedb44f758f5dbad587f8
correiarangel <correiarangel@bol.com.br>
2024-12-08 22:20:31 -0300

    chore: edit DockFile

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,27 @@ FROM python:3.10-slim
 # Defina o diretório de trabalho
 WORKDIR /app
 
-# Copie os arquivos do projeto
+# Instalar dependências do sistema (para garantir que as dependências Python sejam instaladas corretamente)
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    python3-dev \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copie os arquivos do projeto para o contêiner
 COPY . .
 
-# Instale as dependências
+# Instale as dependências do Python
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Exponha a porta (Railway usará PORT automaticamente)
+# Exponha a porta (Railway usará a variável de ambiente PORT automaticamente)
 EXPOSE 8080
+
+# Definir variáveis de ambiente, incluindo o PORT
+# O Railway automaticamente fornece essas variáveis quando o contêiner é iniciado.
+ENV PORT=8080
 
 # Comando para iniciar o servidor
 CMD ["python", "app.py"]
--- app/app.py
+++ app/app.py
@@ -10,6 +10,36 @@ def create_app():
     # Define o aplicativo Flask
     app = Flask(__name__, static_folder="static")
     
+    # Define a chave secreta para a aplicação
+    app.secret_key = os.getenv("FLASK_SECRET_KEY")  # Agora a chave vem do .env
+    # Inicializa as rotas
+    initialize_routes(app)
+    
+    return app
+
+if __name__ == '__main__':
+    # Cria e executa o aplicativo em modo debug
+    app = create_app()
+    
+    # Obtém a porta do ambiente ou usa a padrão (8080)
+    port = int(os.environ.get('PORT', 8080))
+    
+    # Inicia o servidor Flask na porta configurada
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
+""" from routes import initialize_routes
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+def create_app():
+    # Define o aplicativo Flask
+    app = Flask(__name__, static_folder="static")
+    
   # Define a chave secreta para a aplicação
     app.secret_key = os.getenv("FLASK_SECRET_KEY")  # Agora a chave vem do .env
     # Inicializa as rotas
@@ -22,3 +52,4 @@ if __name__ == '__main__':
     # Cria e executa o aplicativo em modo debug
     app = create_app()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+ """
