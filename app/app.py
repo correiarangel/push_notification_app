5245508ba2d9bcd14fa97f1af05c085e857bf575
correiarangel <correiarangel@bol.com.br>
2024-12-13 09:32:03 -0300

    chore: adjusting in app

--- app/app.py
+++ app/app.py
@@ -4,33 +4,28 @@ from dotenv import load_dotenv
 import os
 import logging
 
-# Configuração do logger use local
-#logging.basicConfig(level=logging.DEBUG)
+# Configuração do logger
+logging.basicConfig(level=logging.DEBUG)
 
 # Carregar variáveis de ambiente
 load_dotenv()
 
 def create_app():
+    """Cria e configura a aplicação Flask"""
     app = Flask(__name__, static_folder="static")
     app.secret_key = os.getenv("FLASK_SECRET_KEY")
     initialize_routes(app)
     logging.info("App created successfully!")
     return app
 
-# A lógica de inicialização do Gunicorn será responsável 
-# pela execução, então removemos app.run().
+# Instância única para uso pelo Gunicorn
 app = create_app()
 
-# Apenas para desenvolvimento, o Gunicorn gerenciará a execução em produção.
-try:
-    logging.basicConfig(level=logging.DEBUG)
-    app = create_app()
-    if __name__ == '__main__':
-        app.run(port=8080, debug=True)
-except Exception as e:
-    logging.error(f"Failed to start the app: {str(e)}")
-    
-    raise
+if __name__ == '__main__':
+    # Execução em modo de desenvolvimento
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
     
     # Obtém a porta do ambiente ou usa a padrão (8080)
   # port = int(os.environ.get('PORT', 8080))
