79afde65cb2f18465ba9b53be6e4ef60367be9ae
correiarangel <correiarangel@bol.com.br>
2024-12-13 21:41:13 -0300

    fix: Perhaps out of memory b

--- app/gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-import os
-import multiprocessing
-
-bind = "0.0.0.0:8080" # Porta fixa dentro do contêiner
-workers = multiprocessing.cpu_count() 
-timeout = int(os.environ.get("GUNICORN_TIMEOUT", 60))
-worker_class = "gthread"
